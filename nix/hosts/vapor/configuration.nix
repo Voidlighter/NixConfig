4bbc26160f259afaddb0aae5b9a735ac65bfdc58
voidlighter <voidlighter@proton.me>
2025-01-02 13:33:42 -0700

    idk maybe this'll work?

--- nix/hosts/vapor/configuration.nix
+++ nix/hosts/vapor/configuration.nix
@@ -31,18 +31,32 @@
   #     };
   #   };
 
-  nix.distributedBuilds = true;
-  nix.settings.builders-use-substitutes = true;
-
+  # You can see the resulting builder-strings of this NixOS-configuration with "cat /etc/nix/machines".
+  # These builder-strings are used by the Nix terminal tool, e.g.
+  # when calling "nix build ...".
   nix.buildMachines = [
     {
+      # Will be used to call "ssh builder" to connect to the builder machine.
+      # The details of the connection (user, port, url etc.)
+      # are taken from your "~/.ssh/config" file.
       hostName = "veridia";
-      sshUser = "remotebuild";
-      sshKey = "/root/.ssh/remote-build-vapor";
-      system = pkgs.stdenv.hostPlatform.system;
-      supportedFeatures = ["nixos-test" "big-parallel" "kvm"];
+      system = "x86_64-linux";
+      # Nix custom ssh-variant that avoids lots of "trusted-users" settings pain
+      protocol = "ssh-ng";
+      # default is 1 but may keep the builder idle in between builds
+      maxJobs = 3;
+      # how fast is the builder compared to your local machine
+      speedFactor = 2;
+      supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+      mandatoryFeatures = [];
     }
   ];
+  # required, otherwise remote buildMachines above aren't used
+  nix.distributedBuilds = true;
+  # optional, useful when the builder has a faster internet connection than yours
+  nix.settings = {
+    builders-use-substitutes = true;
+  };
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
