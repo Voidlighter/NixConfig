c5dc9d2f10b3f64d07750b0f816e4906bb435abe
voidlighter <voidlighter@proton.me>
2025-01-02 10:31:34 -0700

    fixed missing display manager

--- nix/hosts/common.nix
+++ nix/hosts/common.nix
@@ -70,6 +70,8 @@
 
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.sddm.enable = true;
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
