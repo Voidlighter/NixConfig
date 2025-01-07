aa13aefbf2df63339aa9ca3fa6f24654eb9f182c
voidlighter <voidlighter@proton.me>
2025-01-06 21:59:34 -0700

    removed starship config

--- nix/home-manager/common.nix
+++ nix/home-manager/common.nix
@@ -95,20 +95,6 @@
     nix-index.enableZshIntegration = true;
     #     nix-index-database.comma.enable = true;
 
-    starship.enable = true;
-    starship.settings = {
-      aws.disabled = true;
-      gcloud.disabled = true;
-      kubernetes.disabled = false;
-      git_branch.style = "242";
-      directory.style = "blue";
-      directory.truncate_to_repo = false;
-      directory.truncation_length = 8;
-      python.disabled = true;
-      ruby.disabled = true;
-      hostname.ssh_only = false;
-      hostname.style = "bold green";
-    };
     alacritty = {
       enable = true;
       # custom settings
