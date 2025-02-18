d4c59f18106b9639acc8d7b40ea2796a91e4ffe4
voidlighter <voidlighter@proton.me>
2025-02-17 23:30:09 -0700

    updated hardware.nix for vapor

--- hosts/vapor/hardware.nix
+++ hosts/vapor/hardware.nix
@@ -8,25 +8,23 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" "sdhci_pci" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usbhid" "sdhci_pci" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/b823b9bd-0dbe-49c6-87a0-9467a1398dbb";
+    { device = "/dev/disk/by-uuid/066ab6bf-988d-40c1-bb21-59e631c8c3a2";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DCAC-3D41";
+    { device = "/dev/disk/by-uuid/D6DA-F172";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/81eb215b-3963-47e6-8da6-f636d32fb7bd"; }
-    ];
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
