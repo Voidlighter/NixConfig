5968a7585ddec55d900b65d259a1dbcf4fca6283
voidlighter <voidlighter@proton.me>
2025-03-13 19:46:23 -0600

    mothership stuff

--- hosts/mothership/default.nix
+++ hosts/mothership/default.nix
@@ -2,6 +2,7 @@
   pkgs,
   modulesPath,
   lib,
+  config,
   ...
 }: {
   imports = [
@@ -23,44 +24,45 @@
     ];
 
     environment = {
-      systemPackages = lib.mkForce (config.my.app.set.system ++ with pkgs; [(
-	writeShellScriptBin "nix_installer"
-	''
-	  #!/usr/bin/env bash
-	  set -euo pipefail
-	  gsettings set org.gnome.desktop.session idle-delay 0
-	  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+      systemPackages = lib.mkForce (config.my.app.set.system ++ []);
+    # lib.mkForce (config.my.app.set.system ++ [(
+    #   writeShellScriptBin "nix_installer"
+    #   ''
+    #     #!/usr/bin/env bash
+    #     set -euo pipefail
+    #     gsettings set org.gnome.desktop.session idle-delay 0
+    #     gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
 
-	  if [ "$(id -u)" -eq 0 ]; then
-		  echo "ERROR! $(basename "$0") should be run as a regular user"
-		  exit 1
-	  fi
+    #     if [ "$(id -u)" -eq 0 ]; then
+    #   	  echo "ERROR! $(basename "$0") should be run as a regular user"
+    #   	  exit 1
+    #     fi
 
-	  if [ ! -d "$HOME/NixConfig/.git" ]; then
-		  git clone https://gitlab.com/hmajid2301/NixConfig.git "$HOME/NixConfig"
-	  fi
+    #     if [ ! -d "$HOME/NixConfig/.git" ]; then
+    #   	  git clone https://gitlab.com/hmajid2301/NixConfig.git "$HOME/NixConfig"
+    #     fi
 
-	  TARGET_HOST=$(ls -1 ~/NixConfig/hosts/*/configuration.nix | cut -d'/' -f6 | grep -v iso | gum choose)
+    #     TARGET_HOST=$(ls -1 ~/NixConfig/hosts/*/configuration.nix | cut -d'/' -f6 | grep -v iso | gum choose)
 
-	  if [ ! -e "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix" ]; then
-		  echo "ERROR! $(basename "$0") could not find the required $HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
-		  exit 1
-	  fi
+    #     if [ ! -e "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix" ]; then
+    #   	  echo "ERROR! $(basename "$0") could not find the required $HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
+    #   	  exit 1
+    #     fi
 
-	  gum confirm  --default=false \
-	  "🔥 🔥 🔥 WARNING!!!! This will ERASE ALL DATA on the disk $TARGET_HOST. Are you sure you want to continue?"
+    #     gum confirm  --default=false \
+    #     "🔥 🔥 🔥 WARNING!!!! This will ERASE ALL DATA on the disk $TARGET_HOST. Are you sure you want to continue?"
 
-	  echo "Partitioning Disks"
-	  sudo nix run github:nix-community/disko \
-	  --extra-experimental-features "nix-command flakes" \
-	  --no-write-lock-file \
-	  -- \
-	  --mode zap_create_mount \
-	  "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
+    #     echo "Partitioning Disks"
+    #     sudo nix run github:nix-community/disko \
+    #     --extra-experimental-features "nix-command flakes" \
+    #     --no-write-lock-file \
+    #     -- \
+    #     --mode zap_create_mount \
+    #     "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
 
-	  sudo nixos-install --flake "$HOME/NixConfig#$TARGET_HOST"
-	''
-      )]);
+    #     sudo nixos-install --flake "$HOME/NixConfig#$TARGET_HOST"
+    #   ''
+    # )]);
     };
     
     nixpkgs.hostPlatform = "x86_64-linux";
