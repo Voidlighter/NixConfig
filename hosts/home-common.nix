5c4131d6ec1c5f5f44556909093eff5bf3ce8c55
voidlighter <voidlighter@proton.me>
2025-03-13 19:38:59 -0600

    Crateria setup and Mothership setup

--- hosts/home-common.nix
+++ hosts/home-common.nix
@@ -16,6 +16,7 @@
 
   # config = {
   # users.${osConfig.my.user.name} = {
+  initialPassword = "";
   home = {
     # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
     # basically don't change this. It isn't the version number
@@ -40,6 +41,59 @@
       Type=Application
       Categories=Network;
     '';
+    "install-instructions.md".text = ''
+# Minimal UEFI/GPT Installation
+```
+lsblk
+sudo fdisk /dev/diskX
+```
+In fdisk:
+    g (gpt disk label)
+    n
+    1 (partition number [1/128])
+    2048 first sector
+    +500M last sector (boot sector size)
+    t
+    1 (EFI System)
+    n
+    2
+    default (fill up partition)
+    default (fill up partition)
+    w (write)
+
+# Label Partitions
+
+```
+lsblk
+```
+
+use those labels for the following commands:
+
+```
+sudo mkfs.fat -F 32 /dev/sda1
+sudo fatlabel /dev/sda1 NIXBOOT
+sudo mkfs.ext4 /dev/sda2 -L NIXROOT
+```
+
+# Create a swap file
+
+```
+sudo dd if=/dev/zero of=/mnt/.swapfile bs=1024 count=2097152 # 2GB size
+sudo chmod 600 /mnt/.swapfile
+sudo mkswap /mnt/.swapfile
+sudo swapon /mnt/.swapfile
+```
+
+# Create NixOS config
+
+```
+sudo nixos-generate-config --root /mnt
+sudo cp /mnt/etc/nixos/hardware-configuration.nix ~/NixConfig/hosts/HOST/hardware.nix
+cd /mnt
+sudo nixos-install --flake ~/NixConfig#HOST
+passwd
+```
+      '';
     };
   };
 
--- hosts/mothership/default.nix
+++ hosts/mothership/default.nix
@@ -1,12 +1,14 @@
 {
   pkgs,
   modulesPath,
+  lib,
   ...
 }: {
   imports = [
     ./../common.nix
     ./home.nix
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+    # "${modulesPath}/installer/cd-dvd/iso-image"
   ];
 
   config = {
@@ -20,6 +22,70 @@
       "extraUtils"
     ];
 
+    environment = {
+      systemPackages = lib.mkForce (config.my.app.set.system ++ with pkgs; [(
+	writeShellScriptBin "nix_installer"
+	''
+	  #!/usr/bin/env bash
+	  set -euo pipefail
+	  gsettings set org.gnome.desktop.session idle-delay 0
+	  gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+
+	  if [ "$(id -u)" -eq 0 ]; then
+		  echo "ERROR! $(basename "$0") should be run as a regular user"
+		  exit 1
+	  fi
+
+	  if [ ! -d "$HOME/NixConfig/.git" ]; then
+		  git clone https://gitlab.com/hmajid2301/NixConfig.git "$HOME/NixConfig"
+	  fi
+
+	  TARGET_HOST=$(ls -1 ~/NixConfig/hosts/*/configuration.nix | cut -d'/' -f6 | grep -v iso | gum choose)
+
+	  if [ ! -e "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix" ]; then
+		  echo "ERROR! $(basename "$0") could not find the required $HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
+		  exit 1
+	  fi
+
+	  gum confirm  --default=false \
+	  "🔥 🔥 🔥 WARNING!!!! This will ERASE ALL DATA on the disk $TARGET_HOST. Are you sure you want to continue?"
+
+	  echo "Partitioning Disks"
+	  sudo nix run github:nix-community/disko \
+	  --extra-experimental-features "nix-command flakes" \
+	  --no-write-lock-file \
+	  -- \
+	  --mode zap_create_mount \
+	  "$HOME/NixConfig/hosts/$TARGET_HOST/disks.nix"
+
+	  sudo nixos-install --flake "$HOME/NixConfig#$TARGET_HOST"
+	''
+      )]);
+    };
+    
     nixpkgs.hostPlatform = "x86_64-linux";
+
+    users.users.${config.my.user.name} = {
+      isNormalUser = true;
+      initialPassword = ""; # Set the initial password
+    };
+
+    # Enable the default live user
+    users.users.nixos = {
+      isNormalUser = true;
+      initialPassword = "";
+      extraGroups = ["wheel" "networkmanager"];
+    };
+
+    services.displayManager.autoLogin = {
+      enable = true;
+      user = "nixos";
+    };
+
+    # EFI booting
+    isoImage.makeEfiBootable = true;
+
+    # USB booting
+    isoImage.makeUsbBootable = true;
   };
 }
