0868e945452b080176afec63cf105f6fac4dbec7
cade <cade@voidlighter.org>
2025-10-21 16:00:11 -0600

    elysia cleanup

--- min/elysia/flake.nix
+++ min/elysia/flake.nix
@@ -3,9 +3,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs@{ self, nixpkgs, nixos-hardware, ... }: {
+  outputs = inputs@{ self, nixpkgs, ... }: {
+
     nixosConfigurations.elysia = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs nixos-hardware; };
+      specialArgs = {inherit inputs;};
       system = "x86_64-linux";
       modules = [ ./default.nix ];
     };
